73b7eb4b7571dbb8806fd7dbf5c65a91313b9f13
Luis De Anda <luisdeanda916@outlook.com>
2019-07-09 14:24:00 -0700

    Used .prepend to shit clicked block to top of stack

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,1 +1,12 @@
 // Stretch Assignment
+
+//MOVES BLOCK TO TOP OF STACK
+const rockets = document.querySelectorAll('.block');
+let rocketArray = Array.from(rockets);
+
+rocketArray.forEach(item => item.addEventListener('click', function(e) {
+        item.parentNode.prepend(item);
+    })
+);
+
+
